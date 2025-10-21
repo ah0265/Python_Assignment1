d37bf6fc8e7f6e7930dd8d266cc113e9e1db3c1d
Adithyaa Hariharan <adithyaa.hariharan@gmail.com>
2025-10-21 19:27:45 +0530

    uploading Task1 and Task2

---
+++ basic_math.py
@@ -0,0 +1,16 @@
+# taking input of two numbers
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# sum
+print("\nAddition: ", a+b)
+
+# subtraction
+print("Subtraction: ", a-b)
+
+# multiply
+print("Subtraction: ", a*b)
+
+# divide
+print("Division: ", a/b)
---
+++ greeting.py
@@ -0,0 +1,6 @@
+# input first and last name
+FN = str(input("Enter your first name: "))
+LN = str(input("Enter your last name: "))
+
+# printing greeting
+print(f"Hello, {FN} {LN}! Welcome to the Python program.")
